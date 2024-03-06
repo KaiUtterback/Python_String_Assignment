edfc4ad5b1d33c37b156aba25939baf412696762
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-03-06 09:45:24 -0600

    First Save

---
+++ string_assignment.py
@@ -0,0 +1,8 @@
+'''
+Question 1:
+
+Task 1: Code Correction
+You are provided with a scripot that is supposed to format customer names by ensuring the first letter is
+uppercase and the rest are lowercase, regardless of how the data was entered.
+However, the script contains errors. Correct the script so that it functions as intended.
+'''
